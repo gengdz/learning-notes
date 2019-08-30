1c8d3ebb209b40e73c19e278a5ab75d98fa41074
dezhougeng <1583751445@qq.com>
2019-08-30 23:10:34 +0800

    格式化webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 let path = require('path');
 module.exports = {
-   mode: 'development',  // 模式有两种，分别是production，development，
-   entry: './src/index.js',   // 入口    
-   output: {
-     fileName: 'index.js',    // 打包后的文件名
-     path: path.resolve(__dirname, 'dist'), // 打包后的地址，这里必须是一个绝对路径
+  mode: 'development',  // 模式有两种，分别是production，development，
+  entry: './src/index.js',   // 入口    
+  output: {
+    fileName: 'index.js',    // 打包后的文件名
+    path: path.resolve(__dirname, 'dist'), // 打包后的地址，这里必须是一个绝对路径
 
-   }
+  }
 
 }
 
