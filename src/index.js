ad23ae427b9f4f210bf39d28521bfe83c9ed02ce
dezhougeng <1583751445@qq.com>
2019-09-01 15:24:41 +0800

    修改webpack配置

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import './index.less';
 console.log('webpack文件从这里开始打包')
 console.log('现在我做了修改')
+
+const test = () => {
+    console.log('这是es6的语法')
+}
+test()
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
-let path = require('path');
+const path = require('path');
 // 作用是，将打包后的文件写入到dist,如果没有的话，就新建一个
-let HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = {
-  mode: 'development',  // 模式有两种，分别是production，development，
+  mode: 'production',  // 模式有两种，分别是production，development，
   devServer: { // 开发服务器的配置，作用是可以通过 localhost的方式打开服务，好处是：可以查看打包后的结果，但不会真的进行打包，只会在内存中打包
     port: 3000, // 打开的地址
     progress: true, // 显示打包的进度条
@@ -14,6 +17,9 @@ module.exports = {
     filename: 'index.js',    // 打包后的文件名, 可以加上hash 如： index[hash:8].js
     path: path.resolve(__dirname, 'dist'), // 打包后的地址，这里必须是一个绝对路径
   },
+  optimization: { // 只有在生产模式下才能压缩
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   plugins: [   // 插件数组，
     new HtmlWebpackPlugin({
       template: './src/index.html', // 生成的模板
@@ -23,9 +29,34 @@ module.exports = {
         collapseWhitespace: true, // 压缩空格
       },
       hash: true, // shengcheng hash值
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+
     })
 
-  ]
+  ],
+
+  module: { // 模块配置(js,css,less模块等)
+    rules: [
+      {
+        test: /\.less$/,  // .less结尾的使用这种loader，loader的执行顺序为：从后往前
+        use: [
+          // {
+          //   loader: 'style-loader',
+          //   // options: {
+          //   //   insertAt: 'top'
+          //   // }
+          // },
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ]
+
+      }
+    ]
+  }
+
 
 }
 
