62c288fbc8d3e9deb299e33b398d38ca93242bca
dezhougeng <1583751445@qq.com>
2019-09-01 21:57:45 +0800

    太多配置了!

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = {
   mode: 'production',  // 模式有两种，分别是production，development，
   devServer: { // 开发服务器的配置，作用是可以通过 localhost的方式打开服务，好处是：可以查看打包后的结果，但不会真的进行打包，只会在内存中打包
+    hot: true,
     port: 3000, // 打开的地址
     progress: true, // 显示打包的进度条
     contentBase: './build', // 打包的路径
@@ -40,6 +41,12 @@ module.exports = {
   module: { // 模块配置(js,css,less模块等)
     rules: [
       {
+        test: /\.js$/,  // .less结尾的使用这种loader，loader的执行顺序为：从后往前
+        use: {
+          loader: 'eslint-loader',
+          enforce: 'pre'
+        }
+      }, {
         test: /\.less$/,  // .less结尾的使用这种loader，loader的执行顺序为：从后往前
         use: [
           // {
@@ -52,8 +59,24 @@ module.exports = {
           'css-loader',
           'less-loader',
         ]
-
-      }
+      }, {
+        test: /\.js$/, // normal 普通的loader
+        use: {
+          loader: 'babel-loader',
+          options: { // 用babel-loader 需要把es6-es5
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", { "legacy": true }],
+              ["@babel/plugin-proposal-class-properties", { "loose": true }],
+              "@babel/plugin-transform-runtime"
+            ]
+          }
+        },
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
+      },
     ]
   }
 
