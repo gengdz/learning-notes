2f4554beff13d8867ea38574b4aad4d7290ddcb1
gengdz <1583751445@qq.com>
2020-09-08 09:57:30 +0800

    feat(fp): 自己实现compose

---
+++ 函数式编程/代码示例/compose.ts
@@ -0,0 +1,10 @@
+const compose = (...fns) => (...params) => {
+  return fns.reduceRight((args, fn) => fn(...[].concat(args)), params)
+}
+
+const add = (x: number) => (y: number) => x + y;
+const multiply2 = (x: number) => x * 2;
+const add10 = add(10);
+
+const getData = compose(multiply2, add10)
+console.log('data', getData(2))
