fe8b0fd45066d3365feaebbec942bc9c26ee15c6
gengdz <1583751445@qq.com>
2020-12-17 22:43:41 +0800

    feat(fp): Composable Functional 总结

---
+++ fp/Composable Functional Javascript.md
@@ -0,0 +1,39 @@
+# Composable Functional Javascript
+> 本篇内容主要是参考自
+> * [Professor Frisby Introduces Composable Functional JavaScript](https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript)
+> * [深入学习javascript函数式编程](https://juejin.cn/post/6844903743499026446)
+> * [JavaScript 函数式编程（三）](https://juejin.cn/post/6844903655397834765)
+
+
+
+## 容器（Box)
+自己实现一个 Box
+```javascript
+// 自己实现一个类似Array的Box
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`
+})
+
+// of 方法
+Box.of = x => Box(x);
+```
+> 函数式编程一般约定，函子有一个 of 方法，用来生成新的容器
+```javascript
+Box(1) === Box.of(1)
+```
+
+下面就这么这么用
+```javascript
+const nextCharForNumberString = str =>
+  Box(str)
+    .map(s => s.trim())
+    .map(s => parseInt(s))
+    .map(i => i + 1)
+    .map(i => String.fromCharCode(i))
+    .inspect();
+
+const result = nextCharForNumberString(' 64');
+console.log(result); // "Box(A)"
+```
---
+++ fp/代码示例/Composable Functional Javascript/Box.ts
@@ -0,0 +1,86 @@
+namespace A {
+  function nextCharForNumberString(str) {
+    const trimmed = str.trim();
+    const number = parseInt(trimmed);
+    const nextNumber = number + 1;
+    return String.fromCharCode(nextNumber);
+  }
+
+  const result = nextCharForNumberString(' 64');
+  console.log(result); // "A"
+}
+
+
+namespace B {
+  // 借助 Array
+  const nextCharForNumberString = str =>
+    [str]
+      .map(s => s.trim())
+      .map(s => parseInt(s))
+      .map(i => i + 1)
+      .map(i => String.fromCharCode(i));
+
+  const result = nextCharForNumberString(' 64');
+  console.log(result); // ["A"]
+}
+
+
+namespace C {
+  // 自己实现一个类似Array的Box
+  const Box = x => ({
+    map: f => Box(f(x)),
+    fold: f => f(x),
+    inspect: () => `Box(${x})`
+  })
+
+  const nextCharForNumberString = str =>
+    Box(str)
+      .map(s => s.trim())
+      .map(s => parseInt(s))
+      .map(i => i + 1)
+      .map(i => String.fromCharCode(i))
+      .inspect();
+
+  const result = nextCharForNumberString(' 64');
+  console.log(result); // "Box(A)"
+
+}
+
+namespace C {
+  // 使用 of 将值放入 Functor
+  const Box = x => ({
+    map: f => Box(f(x)),
+    fold: f => f(x),
+    inspect: () => `Box(${x})`
+  })
+  Box.of = x => Box(x);
+
+  const result = Box.of(' 64')
+    .map(s => s.trim())
+    .map(s => parseInt(s))
+    .map(i => i + 1)
+    .map(i => String.fromCharCode(i))
+    .inspect();
+
+  console.log(result); // "Box(A)"
+
+}
+
+
+namespace D {
+  // Box 不仅可以放值，还可以放函数
+  const Box = x => ({
+    map: f => Box(f(x)),
+    fold: f => f(x),
+    inspect: () => `Box(${x})`
+  })
+
+  const f0 = x => x * 100;
+  const add1 = f => x => f(x) + 1;
+  const add2 = f => x => f(x) + 2;
+  const g = Box(f0).map(add1).map(add2).fold(f => f);
+  const res = g(1);
+  console.log(res)
+}
+
+
