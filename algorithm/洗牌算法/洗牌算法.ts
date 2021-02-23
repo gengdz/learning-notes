644ea319c000f922712e7e2c00ead0a442a678d8
gengdz <1583751445@qq.com>
2021-02-23 16:42:41 +0800

    chore(algorithm): 洗牌算法修改备注

--- algorithm/洗牌算法/洗牌算法.ts
+++ algorithm/洗牌算法/洗牌算法.ts
@@ -2,12 +2,13 @@
  * 洗牌算法
  * @param data 
  * @description 先从数组末尾开始，选定最后一个元素，与数组中随机一个位置的元素交换位置
- * 然后往前挪动一个位置，再与数组中的随机一个位置的元素交换位置。
+ * 然后在已经排好的，「除了最后一个元素」 的位置中,随机产生一个位置，让该位置元素与倒数第二个元素进行交换，
+ * 依次类推
  */
 const shuffle = (data: any[]) => {
   return data.reduceRight((acc, cur, index) => {
-    console.log('iii', index)
     const point = Math.floor(Math.random() * (index + 1));
+    // console.log('iii', index, point)
     [acc[point], acc[index]] = [data[index], data[point]];
     return acc;
   }, data)
