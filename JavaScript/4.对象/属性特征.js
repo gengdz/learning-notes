738dba541c7e197d713c907482b2e8d7ae22d7e5
gengdz <1583751445@qq.com>
2021-07-12 11:22:06 +0800

    chore(JS): 对象和数组补充

--- JavaScript/4.对象/属性特征.js
+++ JavaScript/4.对象/属性特征.js
@@ -1,7 +1,12 @@
 const obj = {
-  name: 'gdz'
+  name: 'gdz',
+  age: 27
 }
 
 const desc = Object.getOwnPropertyDescriptor(obj, 'name')
-console.log(JSON.stringify(desc, null, 2))
+console.log(JSON.stringify(desc, null, 2));
+
+
+const descs = Object.getOwnPropertyDescriptors(obj);
+console.log(descs)
 
