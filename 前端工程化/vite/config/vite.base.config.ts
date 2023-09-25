b597fb93d24d7e09f7a144b0aa6f9041ea89f05e
gengdz <1583751445@qq.com>
2023-09-26 00:16:19 +0800

    feat: vite 处理 css

---
+++ 前端工程化/vite/config/vite.base.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+
+const postcssPresetEnv = () => {};
+
+defineConfig(({ command, mode }) => {
+  return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode), // 往全局上挂变量
+    },
+    css: {
+      devSourcemap: true, // 是否生成类名的映射
+      modules: {
+        localsConvention: "camelCaseOnly", // 在 JS 文件只能通过驼峰的方式引用 CSS 类名
+        generateScopedName: "[name]-[local]-[hash:base64:5]", // 生成的类名格式
+        globalModulePaths: [/path\/to\/legacy-styles/], // 不想参与到css模块的路径
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "theme/variables.scss";`,
+        },
+        less: {
+          math: "always", // 怎么解析算术 默认只有 padding: (100px / 2) 会被解析，padding: 100px / 2 不会被解析。 always 就是都要做解析
+          // 全局变量
+          globalVars: {
+            mainColor: "red",
+          },
+        },
+      },
+      postcss: {
+        plugins: [postcssPresetEnv()],
+      },
+    },
+  };
+});
