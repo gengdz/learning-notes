24bc6bd8931d50e812cf9339279d6b3c47d9dde6
gengdz <1583751445@qq.com>
2023-11-07 10:02:00 +0800

    feat(Shell): sh, set -e , if

--- assistance/Shell/hello.sh
+++ assistance/Shell/hello.sh
@@ -1,22 +1,28 @@
 #! /bin/bash
 
 name="gengdezhou"
-echo $name
+# echo $name
+#
+# array=('a' 'b' 'c')
+#
+# echo "第一个元素为：${array[1]}"
+#
+# echo "接收的参数 ${1} 、  ${2}"
+#
+# echo $#
+# echo $*
+#
+# # echo "-- \$@ 演示 ---"
+# # for i in "$@"; do
+# # 	echo $i
+# # done
+#
+# read "?请输入你的名字: " userName
+#
+# echo $userName
 
-array=('a' 'b' 'c')
-
-echo "第一个元素为：${array[1]}"
-
-echo "接收的参数 ${1} 、  ${2}"
-
-echo $#
-echo $*
-
-# echo "-- \$@ 演示 ---"
-# for i in "$@"; do
-# 	echo $i
-# done
-
-read "?请输入你的名字: " userName
-
-echo $userName
+if [ -e $name ]; then
+	echo "name is empty"
+else
+	echo "my name is $name"
+fi
