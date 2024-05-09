b00c5b24e460533edd28757c2e054603c93f0e1a
gengdz <1583751445@qq.com>
2024-05-09 13:01:12 +0800

    feat(TypeScript): 工具泛型

--- src/TypeScript/示例代码/index.ts
+++ src/TypeScript/示例代码/index.ts
@@ -1,3 +1,5 @@
+export {};
+
 // 联合类型
 type StringOrNumber = string | number;
 
@@ -7,6 +9,23 @@ export { StringOrNumber, consoleLog };
 
 let data: unknown;
 data = true; // ok
-data = 2222  // ok
+data = 2222; // ok
 
-let data2: string = data // error
+const data2: string = data; // error
+
+type Fn = <T extends object, K extends keyof T>(obj: T, key: K) => T[K];
+const getObjProp: Fn = (obj, key) => obj[key];
+
+getObjProp({ name: 'xx', age: 10 }, 'ages'); // 类型报错
+
+getObjProp({ name: 'xx' }, 'name'); // 类型不报错
+
+interface IParams {
+  name: string;
+  age: string;
+  c: number[];
+}
+
+const stringfy = (obj: IParams): string => {
+  return '';
+};
