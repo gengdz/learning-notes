b2ee7efbfc04dfdbf21de2cb4d61f068f52fb037
gengdz <1583751445@qq.com>
2024-07-19 10:36:24 +0800

    feat(ES): string

--- src/JavaScript/ECMAScript/2.基本类型/String/1.js
+++ src/JavaScript/ECMAScript/2.基本类型/String/1.js
@@ -1,11 +1,15 @@
-const str = 'gengdezhou'
+const str = 'gengdezhou';
 
-console.log(str.substring(2, 5), str) //=> ngd gengdezhou // 包括开始不包括结束，不会对
-console.log(str.substring(2), str) //=> ngdezhou
+console.log(str.substring(2, 5), str); //=> ngd gengdezhou // 包括开始不包括结束，不会对
+console.log(str.substring(2), str); //=> ngdezhou
 
-console.log(str.substr(2, 3), str) //=> ngd gengdezhou
-console.log(str.substr(2), str) //=> ngdezhou gengdezhou
-
+console.log(str.substr(2, 3), str); //=> ngd gengdezhou
+console.log(str.substr(2), str); //=> ngdezhou gengdezhou
 
 console.log(str.padStart(100, '   xx  '), str);
 console.log(str.padEnd(100, '   xx  '), str);
+
+let text3 = 'John Smith';
+let newText3 = text3.replace(/(\w+)\s(\w+)/, (match, p1, p2) => {
+  return `${p2}, ${p1}`;
+});
