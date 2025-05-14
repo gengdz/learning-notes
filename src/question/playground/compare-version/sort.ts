74ab48275d5b5fb0f5c5212e7902fde4f54dde66
gengdz <1583751445@qq.com>
2025-05-14 20:52:18 +0800

    feat(question): 版本号比较

---
+++ src/question/playground/compare-version/sort.ts
@@ -0,0 +1,26 @@
+export {};
+// 示例：6.0 > 5.0.2 > 5.0.1 > 5.0.1.beta.3 > 5.0.0
+// 输入为 const versions = ['5.0.0', '5.0.2', '6.0', '5.0.1.beta.3', '5.0.1'];
+// 输出为 const sortedVersions = ['6.0', '5.0.2', '5.0.1', '5.0.1.beta.3', '5.0.0'];
+
+const versions = ['5.0.0', '5.0.2', '6.0', '5.0.1.beta.3', '5.0.1'];
+
+const compareVersion = (v1: string, v2: string) => {
+  const parts1 = v1.split('.');
+  const parts2 = v2.split('.');
+
+  for (let i = 0; i < Math.max(parts1.length, parts2.length); i++) {
+    const num1 = parts1[i] === 'beta' ? -1 : parseInt(parts1[i]) || 0;
+    const num2 = parts2[i] === 'beta' ? -1 : parseInt(parts2[i]) || 0;
+
+    if (num1 !== num2) {
+      return num1 - num2 > 0 ? 1 : -1;
+    }
+  }
+  return 0;
+};
+
+const sortVersioons = (versions: string[]) =>
+  versions.sort((a, b) => compareVersion(b, a));
+
+console.log('sortVersioons--->', sortVersioons(versions));
